e6723363c7671cf2472d0af46691a069da663783
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2022-11-29 13:26:07 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e)=>{
+      e.preventDefault()
+      handleToDo(e.target.newTaskDescription.value)
+      form.reset()
+  } )
 });
+function handleToDo(task){
+ console.log(task)
+ let p = document.createElement("p")
+ let btn = document.createElement("button") 
+ btn.addEventListener("click", handleDelete)
+ btn.textContent= "del"
+ p.textContent = ` ${task} `
+ p.appendChild(btn)
+ console.log(p)
+ document. querySelector("#tasks").appendChild(p)
+}
+ function handleDelete(e){
+  e.target.parentNode.remove()
+ }
